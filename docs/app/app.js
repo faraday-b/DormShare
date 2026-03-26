6d0c4fb6c878058264d615b463e446dcf2fe5698
Christian Appiah <christianaac10@gmail.com>
2026-03-26 10:22:01 +0000

    test details page

--- docs/app/app.js
+++ docs/app/app.js
@@ -4,45 +4,77 @@ const express = require("express");
 // Create express app
 var app = express();
 
+// Use the Pug templating engine
+app.set("view engine", "pug");
+app.set("views", "./app/views");
+
 // Add static files location
 app.use(express.static("static"));
+app.use(express.urlencoded({ extended: true }));
 
 // Get the functions in the db.js file to use
-const db = require('./services/db');
+const db = require("./services/db");
+
+// Import item routes
+const itemsRoutes = require("./routes/items");
+
+// Use item routes
+app.use("/items", itemsRoutes);
 
 // Create a route for root - /
-app.get("/", function(req, res) {
-    res.send("Hello world!");
+app.get("/", function (req, res) {
+    res.redirect("/items/1");
 });
 
 // Create a route for testing the db
-app.get("/db_test", function(req, res) {
-    // Assumes a table called test_table exists in your database
-    sql = 'select * from test_table';
-    db.query(sql).then(results => {
-        console.log(results);
-        res.send(results)
-    });
+app.get("/db_test", function (req, res) {
+    let sql = "select * from test_table";
+    db.query(sql)
+        .then(results => {
+            console.log(results);
+            res.send(results);
+        })
+        .catch(err => {
+            console.error("Database test failed:", err);
+            res.status(500).send("Database test failed");
+        });
+});
+
+// User profile route: /user/:id/profile
+app.get("/user/:id/profile", function (req, res) {
+    var userId = req.params.id;
+
+    var sql = "SELECT id, username, points FROM users WHERE id = ?";
+
+    db.query(sql, [userId])
+        .then(results => {
+            if (results.length > 0) {
+                res.render("profile", {
+                    pageTitle: results[0].username + " - DormShare",
+                    data: results[0]
+                });
+            } else {
+                res.status(404).send("User not found in our system.");
+            }
+        })
+        .catch(err => {
+            console.error("Database error:", err);
+            res.status(500).send("Internal Server Error");
+        });
 });
 
 // Create a route for /goodbye
-// Responds to a 'GET' request
-app.get("/goodbye", function(req, res) {
+app.get("/goodbye", function (req, res) {
     res.send("Goodbye world!");
 });
 
-// Create a dynamic route for /hello/<name>, where name is any value provided by user
-// At the end of the URL
-// Responds to a 'GET' request
-app.get("/hello/:name", function(req, res) {
-    // req.params contains any parameters in the request
-    // We can examine it in the console for debugging purposes
+// Create a dynamic route for /hello/<name>
+app.get("/hello/:name", function (req, res) {
     console.log(req.params);
-    //  Retrieve the 'name' parameter and use it in a dynamically generated page
     res.send("Hello " + req.params.name);
 });
 
 // Start server on port 3000
-app.listen(3000,function(){
-    console.log(`Server running at http://127.0.0.1:3000/`);
+app.listen(3000, function () {
+    console.log("Server running at http://127.0.0.1:3000/");
 });
